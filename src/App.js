2032b1658bc8a005820ded43903204e2b2ae0803
Saliha <salihaoguz2011@gmail.com>
2023-02-23 16:37:25 +0000

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import data from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+
+      {data.map((item) => (
+        <Main {...item} />
+      ))}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,19 @@
+import "./Main.css";
+
+const Main = (props) => {
+  const { img, title, desc } = props;
+  return (
+    <div className="main">
+      <div className="card">
+        <img src={img} alt="" className="mainImg" />
+        <h4>{title}</h4>
+        <div className="desc">
+          <p>{desc}</p>
+        </div>
+        <button className="btn">More Details</button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
